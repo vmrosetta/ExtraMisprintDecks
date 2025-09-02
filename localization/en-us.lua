b3fead243af24405593289a6faec5b3f5820d243
Rosetta Stephens <rastephens426@gmail.com>
2025-09-01 21:24:18 -0700

    my brain is soup

---
+++ localization/en-us.lua
@@ -0,0 +1,68 @@
+return {
+    descriptions = {
+        Back = {
+            b_exmis_extreme = {
+                name = "Extreme Misprinted Deck",
+                text = {
+                    "Misprintizes values between", "{s:1.1}0.005x and 200x{}", "of their original values"
+                }
+            },
+            b_exmis_maxima = {
+                name = "Maxima Misprinted Deck",
+                text = {
+                    "Misprintizes values between", "{s:1.2}0.0000005x and 2000000x{}", "of their original values", "{s:0.85,C:inactive}Watch out! You're gonna crash! Ahhh!{}"
+                }
+            },
+            b_exmis_red = {
+                name = "Misprinted Red Deck",
+                text = {
+                    "May {C:attention}permanently{} give or take", "a {C:red}discard{} at {C:attention,E:1}random{}", "when selecting a blind"
+                }
+            },
+            b_exmis_blue = {
+                name = "Misprinted Blue Deck",
+                text = {
+                    "May {C:attention}permanently{} give or take", "a {C:blue}hand{} at {C:attention,E:1}random{}", "when selecting a blind"
+                }
+            },
+            b_exmis_plasma = {
+                name = "Misprinted Plasma Deck",
+                text = {
+                    "Balances...? {C:blue}Chips{} and {C:red}Mult{}", "at end of scoring"
+                }
+            },
+            b_exmis_erratic = {
+                name = "Misprinted Erratic Deck",
+                text = {
+                    "Randomizes ranks and suits every ante"
+                }
+            }
+        },
+        Sleeve = {
+            sleeve_exmis_extreme_sleeve = {
+                name = "Extreme Misprinted Sleeve",
+                text = {
+                    "Misprintizes values between", "{s:1.1}0.005x and 200x{}", "of their original values"
+                }
+            },
+            sleeve_exmis_maxima_sleeve = {
+                name = "Maxima Misprinted Sleeve",
+                text = {
+                    "Misprintizes values between", "{s:1.2}0.0000005x and 2000000x{}", "of their original values", "{s:0.85,C:inactive}Watch out! You're gonna crash! Ahhh!{}"
+                }
+            },
+            sleeve_exmis_erratic_sleeve = {
+                name = "Misprinted Erratic Sleeve",
+                text = {
+                    "Randomizes ranks and", "suits every ante"
+                }
+            },
+            sleeve_exmis_erratic_sleeve_alt = {
+                name = "Misprinted Erratic Sleeve",
+                text = {
+                    "Randomizes ranks, suits,", "enhancements, editions and", "seals every ante"
+                }
+            }
+        }
+    }
+}
